7d39996f00e1dbb3bc6d7927e63742bc9a6c0c7e
KaeYan <kaeyan.goh@ncs.com.sg>
2019-11-09 21:18:56 +0800

    feat: docker and kubernetes builds

---
+++ course-03/exercises/udacity-c3-restapi-user/src/config/config.ts
@@ -0,0 +1,23 @@
+export const config = {
+  "dev": {
+    "username": process.env.UDAGRAM_USERNAME,
+    "password": process.env.UDAGRAM_PASSWORD,
+    "database": process.env.UDAGRAM_DB,
+    "host": process.env.UDAGRAM_HOST,
+    "dialect": process.env.UDAGRAM_DIALECT,
+    "aws_region": process.env.UDAGRAM_REGION,
+    "aws_profile": process.env.UDAGRAM_PROFILE,
+    "aws_media_bucket": process.env.UDAGRAM_AWS_MEDIA_BUCKET
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt": {
+    "secret": "helloworld"    
+  }
+
+}
